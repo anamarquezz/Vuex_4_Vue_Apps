26257318611bc1d09f3f5c73de989d7c47bf9689
ana_marquez <anamarquez.edu@hotmail.com>
2020-02-10 09:16:29 -0800

    Display items in the UI

--- shopping-list/src/views/ShoppingList.vue
+++ shopping-list/src/views/ShoppingList.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h3> Add Item</h3>       
         <table class="pure-table">
             <thead>
                 <th>Item Name</th>
@@ -7,13 +8,30 @@
                 <th>Quantity</th>
             </thead>
             <tbody>
-               <tr v-for="item in $store.state.shoppingList" :key="item.id">
-          <td>{{ item.name }}</td>
-          <td>{{ item.price }}</td>
-          <td>{{ item.quantity }}</td>
-        </tr>
+                <tr v-for="item in $store.state.shoppingList" :key="item.id">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.price }}</td>
+                    <td>{{ item.quantity }}</td>
+                </tr>
             </tbody>
         </table>
         <h3>Shopping List</h3>
     </div>
 </template>
+<script>
+    const INITIAL_ITEM = {
+        name: "",
+        price: null,
+        quantity: 1
+    }
+    export default {
+        data() {
+            return {
+                newItem: Object.assign({}, INITIAL_ITEM)
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
